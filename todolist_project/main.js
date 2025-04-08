c05bfc4238730a02b13e269643cadfb62dfad10b
Faraaz Kurawle <faraaz@kurawle>
2025-04-08 16:34:22 +0530

    Add two lines for design purpose
    the hline is static but vline is dynamic and its heigh changes for each new item

--- todolist_project/main.js
+++ todolist_project/main.js
@@ -1,9 +1,18 @@
 const items_container = document.getElementById("items");
 const text_area = document.getElementById("text_area");
+const vline = document.getElementById("vline");
+const hline = document.getElementById("hline");
 
 function add_item(){
     var item = document.createElement("p");
     item.setAttribute("class", "item")
+    
     items_container.appendChild(item)
     item.innerText = text_area.value;
+
+    var rect = item.getBoundingClientRect()
+    vline.style.height= `${rect.y}px`;
+
+    vline.style.backgroundColor = "black";
+    hline.style.backgroundColor = "black";
 }
