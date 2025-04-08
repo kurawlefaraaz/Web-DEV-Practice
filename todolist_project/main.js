f61fe45f68cb2a8f2d6d5fb65fc61dbf96893ea7
Faraaz Kurawle <faraaz@kurawle>
2025-04-08 18:32:57 +0530

    Removed Unnecessary lines

--- todolist_project/main.js
+++ todolist_project/main.js
@@ -5,14 +5,8 @@ const vline = document.getElementById("vline");
 const lines = document.getElementsByClassName("theline");
 
 var index = 1;
-var arrItems = [];
 
 function add_item(){
-    var item_index = document.createElement("span");
-    item_index.setAttribute("class", `item_index ${index}`);
-    item_index.innerText = `${index}. `
-    items_container.appendChild(item_index);
-
     var item_line = document.createElement("span");
     item_line.setAttribute("class", `item_line ${index}`);
     items_container.appendChild(item_line);
@@ -25,7 +19,6 @@ function add_item(){
     var rect = item.getBoundingClientRect();
     vline.style.height= `${rect.y - 10}px`;
     item_line.style.top= `${rect.y + 10}px`;
-    item_index.style.top= `${rect.y + 5}px`;
     
     for (let i = 0; i < lines.length; i++) {
         lines[i].style.backgroundColor = "black";
