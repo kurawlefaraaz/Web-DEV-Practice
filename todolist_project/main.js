0d2b5a609ce2310277ad3ec0aead92fce591692f
Faraaz Kurawle <faraaz@kurawle>
2025-04-09 00:54:12 +0530

    fix improper position of thelines

--- todolist_project/main.js
+++ todolist_project/main.js
@@ -4,35 +4,69 @@ const text_area = document.getElementById("text_area");
 const vline = document.getElementById("vline");
 const lines = document.getElementsByClassName("theline");
 
-var index = 1;
+var item_index = 1;
 
 const error_div = document.getElementById("error");
-error_div.innerText = "Error: Empty Item is prohibhited."
-error_div.remove()
 
 function add_item(){
     if (text_area.value == ""){
-        document.body.appendChild(error_div);
-        setTimeout(() => error_div.remove(), 2000);
+        error_div.innerText = "Error: Empty Item is prohibhited."
+        setTimeout(() => error_div.innerText = "", 2000);
         return 1;
     }
+    var container = document.createElement("div");
 
     var item_line = document.createElement("span");
-    item_line.setAttribute("class", `item_line ${index}`);
-    items_container.appendChild(item_line);
+    item_line.setAttribute("class", `item_line`);
+    item_line.addEventListener("contextmenu", remove_item)
+    item_line.addEventListener("click", item_done)
+    container.appendChild(item_line);
 
-    var item = document.createElement("div");
-    item.setAttribute("class", `item ${index}`);
+    var item = document.createElement("span");
+    item.setAttribute("class", `item`);
     item.innerText = text_area.value;
-    items_container.appendChild(item);
+    item.addEventListener("contextmenu", remove_item)
+    item.addEventListener("click", item_done)
+    container.appendChild(item);
+
+    items_container.appendChild(container);
 
     var rect = item.getBoundingClientRect();
-    vline.style.height= `${rect.y - 10}px`;
-    item_line.style.top= `${rect.y + 10}px`;
+    vline.style.height= `${rect.y - 20}px`;
     
     for (let i = 0; i < lines.length; i++) {
         lines[i].style.backgroundColor = "black";
     }
 
-    index++;
-} 
+    item_index++;
+}
+
+function remove_item(e){
+    var ele = e.target;
+    ele = ele.parentNode;
+    ele.remove()
+
+    var lastc = items_container.lastChild
+    if (lastc){
+        var rect = lastc.getBoundingClientRect();
+        vline.style.height= `${rect.y - 20}px`;
+        vline.style.textDecorationLine= `${rect.y - 20}px`;
+    } else{
+        for (let i = 0; i < lines.length; i++) {
+            lines[i].style.backgroundColor = "white";
+        }
+    }
+}
+
+function item_done(e){
+    var ele = e.target;
+    ele = ele.parentNode;
+    ele.lastChild.style.textDecorationThickness = "3px";
+    console.log(ele.lastChild.style.textDecorationLine);
+    if (ele.lastChild.style.textDecorationLine == "line-through"){
+        ele.lastChild.style.textDecorationLine = "none";
+    } else {
+        ele.lastChild.style.textDecorationLine = "line-through";
+    }
+    // ele.lastChild.style.text = "line-through";
+}
