d506ba67260b28d535a8a49d60386cbf45e99f64
Faraaz Kurawle <faraaz@kurawle>
2025-04-08 16:59:45 +0530

    fix BUG causing `theline` to not set bgcolor as black
    Occured due to improper HTML Collection Handling.

--- todolist_project/main.js
+++ todolist_project/main.js
@@ -1,18 +1,24 @@
 const items_container = document.getElementById("items");
 const text_area = document.getElementById("text_area");
+
 const vline = document.getElementById("vline");
-const hline = document.getElementById("hline");
+const lines = document.getElementsByClassName("theline");
+
+var index = 0;
+var arrItems = [];
 
 function add_item(){
     var item = document.createElement("p");
-    item.setAttribute("class", "item")
+    item.setAttribute("class", "item");
     
-    items_container.appendChild(item)
+    items_container.appendChild(item);
     item.innerText = text_area.value;
+    arrItems.app
 
-    var rect = item.getBoundingClientRect()
+    var rect = item.getBoundingClientRect();
     vline.style.height= `${rect.y}px`;
-
-    vline.style.backgroundColor = "black";
-    hline.style.backgroundColor = "black";
-}
+    
+    for (let i = 0; i < lines.length; i++) {
+        lines[i].style.backgroundColor = "black";
+    }
+} 
